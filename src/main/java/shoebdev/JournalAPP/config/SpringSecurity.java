344383d209335378ccb4ff7d9089f4de96c9ee6b
shoeb <wiqbal091@gmail.com>
2026-01-18 16:41:34 +0530

    unwanted import removed

--- src/main/java/shoebdev/JournalAPP/config/SpringSecurity.java
+++ src/main/java/shoebdev/JournalAPP/config/SpringSecurity.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
